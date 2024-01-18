38e5588bab37e85ee1fd62e38d0549f64eabea7e
Lucas Mesquita <lucas.mesquita@perinity.com>
2024-01-17 22:58:40 -0300

    terminando filtro dos pokemons

--- pokedex/src/main/java/com/pokedex/lucas/pokemon/repository/PokemonRepository.java
+++ pokedex/src/main/java/com/pokedex/lucas/pokemon/repository/PokemonRepository.java
@@ -27,13 +27,25 @@ public class PokemonRepository implements PanacheRepository<Pokemon> {
 
 		StringBuilder hql = new StringBuilder();
 		hql.append("select p.id, p.nome, p.numeroDex, ");
-		hql.append("(select string_agg(t.nome, ', ') from tipo t left join rel_pokemontipo rel on rel.idtipo = t.id where rel.idpokemon = p.id) ");
-		hql.append("from pokemon p ");
+		hql.append("(select string_agg(t.nome, ',') FROM rel_pokemontipo r ");
+		hql.append("inner join tipo t on r.idtipo = t.id ");
+		hql.append("WHERE r.idpokemon = p.id) ");
+		hql.append("FROM rel_pokemontipo rel ");
+		hql.append("left join pokemon p ON rel.idpokemon = p.id ");
+		hql.append("left join tipo t ON rel.idtipo = t.id ");
 		hql.append("where 1=1 ");
 
 		if(StringUtils.isNotBlank(filtro.getNome())){
-			hql.append("and p.nome like :nome");
+			hql.append("and p.nome like :nome ");
 			params.put("nome", filtro.getNome());
+		}
+		if(StringUtils.isNotBlank(filtro.getTipo())){
+			hql.append("and t.nome =:tipo ");
+			params.put("tipo", filtro.getTipo());
+		}
+		if(filtro.getGeracao() != null){
+			hql.append("and p.geracao =:geracao ");
+			params.put("geracao", filtro.getGeracao());
 		}
 
 		hql.append("GROUP BY p.id, p.nome ");
@@ -43,7 +55,11 @@ public class PokemonRepository implements PanacheRepository<Pokemon> {
 
 		params.forEach((key, value)->{
 			if(key.equals("nome"))
-				query.setParameter("nome",value);
+				query.setParameter("nome",value + "%");
+			if(key.equals("tipo"))
+				query.setParameter("tipo", value);
+			if(key.equals("geracao"))
+				query.setParameter("geracao", value);
 		});
 
 		return mapToDTO(query.getResultList());
